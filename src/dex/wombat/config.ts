0492403348f7fff517bff8e51ff1251999b6e5b8
warmbread <kenneth@wombat.exchange>
2023-10-19 20:49:25 +0800

    feat: generate wombat config and import it in config.ts

--- src/dex/wombat/config.ts
+++ src/dex/wombat/config.ts
@@ -1,28 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { DexParams } from './types';
 import { DexConfigMap, AdapterMappings } from '../../types';
 import { Network, SwapSide } from '../../constants';
+import wombatConfig from './wombat-config.json';
 
-export const WombatConfig: DexConfigMap<DexParams> = {
-  Wombat: {
-    [Network.BSC]: {
-      pools: [
-        {
-          address: '0x312Bc7eAAF93f1C60Dc5AfC115FcCDE161055fb0',
-          name: 'Wombat Main Pool',
-        },
-        /** @todo add Side and Dynamic pools */
-      ],
-    },
-    [Network.ARBITRUM]: {
-      pools: [
-        {
-          address: '0xc6bc781E20f9323012F6e422bdf552Ff06bA6CD1',
-          name: 'Wombat Main Pool',
-        },
-      ],
-    },
-  },
-};
+export const WombatConfig: DexConfigMap<DexParams> = wombatConfig;
 
 export const Adapters: Record<number, AdapterMappings> = {
   [Network.BSC]: {
