386017c34674260f279d7374940d7701fd7093d0
warmbread <kenneth@wombat.exchange>
2023-10-19 20:48:25 +0800

    feat: add master wombat addresses

---
+++ src/dex/wombat/constants.ts
@@ -0,0 +1,6 @@
+export const MASTER_WOMBAT_ADDRESS: Record<number, string> = {
+  56: '0x489833311676B566f888119c29bd997Dc6C95830',
+  /** @todo uncomment when needed */
+  // 42161: '0x62A83C6791A3d7950D823BB71a38e47252b6b6F4',
+  // 1: '0xC9bFC3eFeFe4CF96877009F75a61F5c1937e5d1a',
+};
